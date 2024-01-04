80c4b755ef5ee8b9d630c51963c97e3a552fc026
altundal19 <altundal19@itu.edu.tr>
2024-01-05 00:43:49 +0300

    Fix edit and create places

--- controller/common_controller.py
+++ controller/common_controller.py
@@ -25,101 +25,139 @@ def is_meaningful_string(text):
 
 @login_required
 def list_places_page():
-    logger.info(f"List places user_id:{session['uid']}")
 
-    res_user_type = session["user_type"]
+    if request.method == "GET":
+        logger.info(f"List places user_id:{session['uid']}")
 
-    if res_user_type == "place_owner":
-        user_id = session["uid"]
-        response = requests.get(f'{API_URL}/GetAllPlaces?user_id={user_id}')
-    else:
-        response = requests.get(f'{API_URL}/GetAllPlaces')
+        res_user_type = session["user_type"]
 
-    status = response.json()['StatusCode']
-    if status == HTTPStatus.OK:
-        msg = f"Places listed successfully"
-        logger.info(msg)
-    else:
-        msg = f"Error failed to list places {HTTPStatus.INTERNAL_SERVER_ERROR}"
-        logger.error(msg)
-        flash(msg)
+        if res_user_type == "place_owner":
+            user_id = session["uid"]
+            response = requests.get(f'{API_URL}/GetAllPlaces?user_id={user_id}')
+        else:
+            response = requests.get(f'{API_URL}/GetAllPlaces')
 
-    place_dict = response.json()['Data'] 
+        status = response.json()['StatusCode']
+        if status == HTTPStatus.OK:
+            msg = f"Places listed successfully"
+            logger.info(msg)
+        else:
+            msg = f"Error failed to list places {HTTPStatus.INTERNAL_SERVER_ERROR}"
+            logger.error(msg)
+            flash(msg)
 
-    # Convert tags to comma seperated string for UI 
-    # Assuming tags will come from db in the following format: ("tagname", rating<float>)
-    for place_id in place_dict.keys():
-        place_tags = place_dict[place_id]["tags"]
-        if place_tags == None: 
-            place_dict[place_id]["tags"] = ""
-        else: 
-            # only send if tag has 1 as rating to UI 
-            tags = ','.join(tag[1] for tag in place_tags if len(tag)==2 and tag[1] == 1 and is_meaningful_string(tag))
-            place_dict[place_id]["tags"] = ','.join(tags)
+        place_dict = response.json()['Data'] 
 
-    tags = requests.get(f'{API_URL}/GetAllTags').json()["Data"]["all_tags"]
-    return render_template("list_places.html", places=place_dict.values(), tags=tags)
+        # Convert tags to comma seperated string for UI 
+        # Assuming tags will come from db in the following format: ("tagname", rating<float>)
+        for place_id in place_dict.keys():
+            
+            response = requests.get(f'{API_URL}/GetTagByPlace?place_id={place_id}')
+            
+            status = response.json()['StatusCode']
+            if status == HTTPStatus.OK:
+                msg = f"Places listed successfully"
+                logger.info(msg)
+            
+                tags = response.json()["Data"]
+                if tags == None:
+                    place_dict[place_id]["tags"] = ""
+                else:
+                    place_dict[place_id]["tags"] = ",".join(tags.keys())
+            else:
+                msg = f"Error failed to list places {HTTPStatus.INTERNAL_SERVER_ERROR}"
+                logger.error(msg)
+                place_dict[place_id]["tags"] = ""
+
+            flash(msg)
+            # else: 
+                # only send if tag has 1 as rating to UI 
+                # tags = ','.join(tag[1] for tag in place_tags if len(tag)==2 and tag[1] == 1 and is_meaningful_string(tag))
+                # place_dict[place_id]["tags"] = ','.join(tags)
+
+        tags = requests.get(f'{API_URL}/GetAllTags').json()["Data"]["all_tags"]
+        
+        # print(place_dict)
+        return render_template("list_places.html", places=place_dict.values(), tags=tags)
 
 @login_required
 def update_places_page():
-    place = Place(  place_id=request.form["place_id"],
-                    place_name=request.form['name'],
-                    main_category=request.form['main_category'],
-                    tags=None,
-                    link=request.form['link'],
-                    user_id=session["uid"])
+        
+    if request.method == "POST":
+        place = Place(  place_id=request.form["place_id"],
+                        place_name=request.form['name'],
+                        main_category=request.form['main_category'],
+                        tags=None,
+                        link=request.form['link'],
+                        user_id=session["uid"])
+        
+        all_tags = requests.get(f'{API_URL}/GetAllTags').json()["Data"]["all_tags"]
+        tag_dict = {}
+        for tag in all_tags:
+            tag_dict[tag] = 0.0
 
-    # only send tags with value 1 to db
-    if request.form['tags'] != '':
-        place.tags = {tag: 1.0 for tag in request.form['tags'].split(",")}
+        if request.form['tags'] is not []:
+            for tag in request.form['tags'].split(","):
+                tag_dict[tag] = 1.0
 
-    place_data = place.to_json()
-    print(place_data)
-    logger.info(f"Update place user_id:{session['uid']}, place:{place_data}")
-    
-    response = requests.post(f'{API_URL}/UpdatePlace', json=place_data)
-    status = response.json()['StatusCode']
+        place.tags = tag_dict
 
-    if status == HTTPStatus.OK:
-        msg = f"Place edited successfully"
-        logger.info(msg)
-        flash(msg)
-    else:
-        msg = f"Error failed to edit place {HTTPStatus.INTERNAL_SERVER_ERROR}"
-        logger.error(msg)
-        flash(msg)
+        place_data = place.to_json()
+        print(place_data)
+        logger.info(f"Update place user_id:{session['uid']}, place:{place_data}")
+        
+        response = requests.post(f'{API_URL}/UpdatePlace', json=place_data)
+        status = response.json()['StatusCode']
 
-    return redirect(url_for("list_places_page"))
+        if status == HTTPStatus.OK:
+            msg = f"Place edited successfully"
+            logger.info(msg)
+            flash(msg)
+        else:
+            msg = f"Error failed to edit place {HTTPStatus.INTERNAL_SERVER_ERROR}"
+            logger.error(msg)
+            flash(msg)
+
+        return redirect(url_for("list_places_page"))
 
 @login_required
 def create_places_page():
-    place = Place( place_name=request.form['name'],
-                    main_category=request.form['main_category'],
-                    tags=None,
-                    link=request.form['link'],
-                    user_id=session["uid"])
-    
-    # only send tags with value 1 to db
-    if request.form['tags'] is not []:
-        place.tags = {tag: 1.0 for tag in request.form['tags'].split(",")}
+
+    if request.method == "POST":
+        place = Place( place_name=request.form['name'],
+                        main_category=request.form['main_category'],
+                        tags=None,
+                        link=request.form['link'],
+                        user_id=session["uid"])
         
-    place_data = place.to_json()
+        all_tags = requests.get(f'{API_URL}/GetAllTags').json()["Data"]["all_tags"]
+        tag_dict = {}
+        for tag in all_tags:
+            tag_dict[tag] = 0.0
 
-    logger.info(f"Create place user_id:{session['uid']}, place:{place_data}")
+        if request.form['tags'] is not []:
+            for tag in request.form['tags'].split(","):
+                tag_dict[tag] = 1.0
 
-    response = requests.post(f'{API_URL}/AddPlace', json=place_data)
-    status = response.json()['StatusCode']
+        place.tags = tag_dict
+        
+        place_data = place.to_json()
 
-    if status == HTTPStatus.OK:
-        msg = f"Place added successfully"
-        logger.info(msg)
-        flash(msg)
-    else:
-        msg = f"Error failed to add place {HTTPStatus.INTERNAL_SERVER_ERROR}"
-        logger.error(msg)
-        flash(msg)
+        logger.info(f"Create place user_id:{session['uid']}, place:{place_data}")
 
-    return redirect(url_for("list_places_page"))
+        response = requests.post(f'{API_URL}/AddPlace', json=place_data)
+        status = response.json()['StatusCode']
+
+        if status == HTTPStatus.OK:
+            msg = f"Place added successfully"
+            logger.info(msg)
+            flash(msg)
+        else:
+            msg = f"Error failed to add place {HTTPStatus.INTERNAL_SERVER_ERROR}"
+            logger.error(msg)
+            flash(msg)
+
+        return redirect(url_for("list_places_page"))
 
 @login_required
 def delete_places_page(place_id):
